85628737aaee7e74f9e6904fa55fac0c9c626771
Andriy Hnativ <gnat1409@gmail.com>
2018-11-01 21:56:57 +0200

    Lab4_c++_class_helicopter

--- Header.h
+++ Header.h
@@ -1,20 +1,57 @@
 #include "pch.h"
+#pragma once
 #include <iostream>
-#include "Header.h"
+#include "string"
 using namespace std;
-int main()
-{
-	cout << "First Helicopter" << endl;
-	Helicopter my_Helicopter;
-	my_Helicopter.enter();
-	my_Helicopter.show();
-	cout << "Second Helicopter" << endl;
-	Helicopter my_Helicopter2;
-	my_Helicopter2.enter();
-	my_Helicopter2.show();
-	cout << "Third Helicopter" << endl;
-	Helicopter my_Helicopter3;
-	my_Helicopter3.enter();
-	my_Helicopter3.show();
 
-}
+class Helicopter
+{
+private:
+	string max_high, name, max_weight;
+public:
+	string fuel_tank;
+	int number_lobs;
+
+	Helicopter() {
+		max_high = "0";
+		name = "0";
+		max_weight = "0";
+		fuel_tank = "0";
+		number_lobs = 0;
+		name = "0";
+		color = "0";
+		age = "0";
+
+	}
+	void enter() {
+		cout << "enter max high =";
+		cin >> max_high;
+		cout << "enter name of helicopter =";
+		cin >> name;
+		cout << "enter max weight =";
+		cin >> max_weight;
+		cout << "enter fuel tank =";
+		cin >> fuel_tank;
+		cout << "enter number of lobs =";
+		cin >> number_lobs;
+
+
+	}
+	void show() {
+		cout << "enter max high =" << max_high << endl;
+		cout << "enter name of helicopter =" << name << endl;
+		cout << "enter max weight =" << max_weight << endl;
+		cout << "enter fuel tank =" << fuel_tank << endl;
+		cout << "enter number of lobs =" << number_lobs << endl;
+
+
+	}
+	~Helicopter() {
+
+
+	}
+protected:
+	string color, age;
+
+
+};
